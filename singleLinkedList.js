9bb7cf3f0de8caabdb2a29c5f92ef3ddaac6ee41
Subodh Sharma <subodhss23@gmail.com>
2019-05-07 23:46:31 -0700

    adding shift property, which makes removing a node from the beginning of a linked list possible

--- singleLinkedList.js
+++ singleLinkedList.js
@@ -23,13 +23,13 @@ class SinglyLinkedList{
         this.length++;
         return this;
     }
-    // traverse(){
-    //     var current = this.head;
-    //     while(current){
-    //         console.log(current.value);
-    //         current = current.next;
-    //     }
-    // }
+    traverse(){
+        var current = this.head;
+        while(current){
+            console.log(current.value);
+            current = current.next;
+        }
+    }
 
 
     // if there is nothing on the list return undefined
@@ -40,6 +40,7 @@ class SinglyLinkedList{
     // Decrement the length of the list by 1
     // return the value of the node removed
 
+    // removing node from the end of a linked list
     pop(){
             if(!this.head) return undefined;   // or this.tail instead of this.head, this.length can be checked if it is empty or null
             var current = this.head;
@@ -51,24 +52,54 @@ class SinglyLinkedList{
             this.tail = newTail;
             this.tail.next = null;
             this.length--;
-            if(this.length === 0){   //making sure that head and tail are emptied when the last item are poped
+            if(this.length === 0){   //making sure that head and tail are emptied when the last item is poped
                 this.head = null;
                 this.tail = null;
             }
             console.log(current);
     }
+
+    //removing a node from the begining of a linked list
+    // shift()
+    // sudocode
+    // if there are no nodes, return undefined
+    // store the current head property in a variable
+    // set the head property to be the current head's next property
+    //making sure that head and tail are emptied when the last item is removed
+    // decrement the length by 1
+
+  
+
+    shift(){
+        if(!this.head) return undefined;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        if(this.length == 0){
+            this.tail = null;
+        }
+        this.length--;
+        return currentHead;
+    }
+
 }
 
 var list = new SinglyLinkedList();
 list.push("Hi ");
 list.push("there ");
 list.push("!");
-list.pop();
-console.log(list);
-list.pop();
-console.log(list);
-list.pop();
-console.log(list);
-list.pop();
 
+// pop operation
+// list.pop();
+// console.log(list);
+// list.pop();
+// console.log(list);
+// list.pop();
+// console.log(list);
+// list.pop();
+
+//shift operation
+// console.log(list.shift());
+// console.log(list.shift());
+// console.log(list.shift());
+// console.log(list);
 
