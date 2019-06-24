b4e6aeb5e29c753d8ee889409e49e32e1d2e894c
subodhss23@gmail.com <subodhss23@gmail.com>
2019-06-24 12:56:12 -0700

    cleaning up insert

--- singleLinkedList.js
+++ singleLinkedList.js
@@ -155,11 +155,10 @@ class SinglyLinkedList{
     // return true
     insert(index, val){
         if(index < 0 || index > this.length) return false;
-        if(index === this.length){
-             this.push(val);
-             return true;
-        }
-        if(index === 0) return this.unshift(val);
+        if(index === this.length) return !!this.push(val);
+        if(index === 0) return !!this.unshift(val);
+            
+  
         var newNode = new Node(val);
         var prev = this.get(index - 1);
         var temp = prev.next;
